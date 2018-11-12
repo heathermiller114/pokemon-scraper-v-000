bf6d74fd5a01de3ad9ed70a017706189afcc2597
Heather Miller <heather.miller114@gmail.com>
2018-11-12 22:06:07 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,5 +3,8 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   def initialize(id: 1, name: "unnamed", type: "untyped", db: @db)
+    @id = id
+    @name = name
+    @type = type
   end
 end
